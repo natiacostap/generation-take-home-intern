f5c3971b183cdbe1d4c2c53b9ddc5dffbc4f47b4
natiacostap <nati_aap@hotmail.com>
2019-08-17 16:25:47 -0400

    googlemaps carga pero con un error de host

---
+++ src/Component/Apikey.jsx
@@ -0,0 +1,3 @@
+const ApiKey = AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A;
+
+export default ApiKey
--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import GoogleMap from './Component/Map'
 /*
 * Use this component as a launching-pad to build your functionality.
 *
@@ -9,6 +9,7 @@ class YourComponent extends Component {
     return (
       <div>
         <h1> Put your solution here!</h1>
+        <GoogleMap></GoogleMap>
       </div>
     );
   }
